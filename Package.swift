cbbec42837eca31c56026be4f46166749a7da912
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-04-27 10:53:05 +0100

    :arrow_up: Upgrading Kitura 2.2.0 -> 2.3.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "api",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.2.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.3.0"),
         .package(url: "https://github.com/yaslab/CSV.swift.git", from: "2.1.0"),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1")
     ],
